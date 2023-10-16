17b5083b6237270cd873a6088def0a15dd81bacd
tombeaussart <tom59880@hotmail.fr>
2023-10-16 19:05:12 +0200

    ReveSoNord site

---
+++ navBar.js
@@ -0,0 +1,9 @@
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
+}
